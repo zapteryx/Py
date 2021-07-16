82c5219286e3a8a0e9d0bf6ed45a17b0a245cdf6
Inquisitives <inquisitivesosu@gmail.com>
2021-07-17 01:04:01 +0800

    change prefix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bot_token = os.environ.get("bot-token")
 from discord.ext import commands
 
 # client = discord.Client()
-bot = commands.Bot(command_prefix = ".")
+bot = commands.Bot(command_prefix=",")
 
 @bot.event
 async def on_ready():
