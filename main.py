8c29b45ab26500d1c5089982885d2ce944cd89d1
Max <max@zptx.icu>
2021-07-21 20:28:22 +0800

    overall cleanup

--- main.py
+++ main.py
@@ -5,29 +5,31 @@ import os
 bot_token = os.environ.get("bot-token")
 
 from discord.ext import commands
+import discord
 
-# client = discord.Client()
+# Create a bot instance with prefix of ,
 bot = commands.Bot(command_prefix=',', help_command=None)
 
+# On connection ready event
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    # Log to console that we've logged in successfully
+    print('Logged in as: {0.user}'.format(bot))
 
+# On message event
 @bot.event
 async def on_message(message):
+    # Checking if message event was triggered by self
     if message.author == bot.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
+    # Process the bot commands after our message actions are done
     await bot.process_commands(message)
 
-
 @bot.command()
-async def hello(ctx):
-    await ctx.send("Hello!")
-
+async def about(ctx):
+    embed = discord.Embed(title="Py", description="a bot made with ❤️ and python\ndeveloped by **zapteryx**, **QuackNoodels121**, **Inquisitives**\nopen source at https://github.com/zapteryx/Py")
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def ping(ctx):
