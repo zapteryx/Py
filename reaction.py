a9e6655f1e17d2181460a415a26707da21339a32
Max Lim <max@zptx.icu>
2021-07-22 14:21:42 +0800

    try fixing positional args error

--- reaction.py
+++ reaction.py
@@ -28,6 +28,6 @@ class Reaction(commands.Cog):
             msg = await self.bot.wait_for('message', check=message_check, timeout=5.0)
             now = datetime.now()
         except asyncio.TimeoutError:
-            await probe.edit("%s, too late!" % ctx.author.mention)
+            await probe.edit(self, "%s, too late!" % ctx.author.mention)
         else:
-            await probe.edit("%s, time: %f" % (ctx.author.mention, (now - start).total_seconds() * 1000))
+            await probe.edit(self, "%s, time: %f" % (ctx.author.mention, (now - start).total_seconds() * 1000))
