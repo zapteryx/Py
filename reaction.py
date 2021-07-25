439ac694e46d918fabe6e8fdcb2678b1c94a9a8b
Max <max@zptx.icu>
2021-07-25 22:37:21 +0800

    change reaction to use reactions, closes #2

--- reaction.py
+++ reaction.py
@@ -12,21 +12,30 @@ class Reaction(commands.Cog):
         self.users = []
     
     @commands.command()
-    async def reaction(self, ctx):
+    async def reaction(self, ctx, arg):
         if ctx.author.id in self.users:
             await ctx.send("can't start while in progress!")
             return
         self.users.append(ctx.author.id)
-        embed = Embed(title="Reaction", description="test your reaction speed!\n\nget ready the word `Py`, and hit `[Send]` when I tell you to!")
-        await ctx.send(embed=embed)
+        multiple = False
+        reactions = ["1️⃣"]
+        if arg.lower() == "multiple":
+            multiple = True
+            reactions.append("2️⃣")
+            reactions.append("3️⃣")
+        embed = Embed(title="Reaction", description="test your reaction speed!\nreact to my message with the reaction when I tell you to!")
+        initial = await ctx.send(embed=embed)
+        for reaction in reactions:
+            initial.add_reaction(reaction)
+        random_reaction = randint(0, len(reaction) - 1)
         time_to_wait = randint(3, 8)
         await asyncio.sleep(time_to_wait)
-        probe = await ctx.send("%s, hit send now!" % ctx.author.mention)
+        probe = await ctx.send("%s, react with %s now!" % ctx.author.mention, reactions[random_reaction])
         start = datetime.now()
-        def message_check(m):
-            return m.content.lower() == "py" and m.author.id == ctx.author.id and m.channel.id == ctx.channel.id
+        def reaction_check(reaction, user):
+            return user.id == ctx.author.id and str(reaction.emoji) == reactions[random_reaction]
         try:
-            msg = await self.bot.wait_for('message', check=message_check, timeout=5.0)
+            await self.bot.wait_for('reaction_add', check=reaction_check, timeout=5.0)
             now = datetime.now()
         except asyncio.TimeoutError:
             await probe.edit(content="%s, too late!" % ctx.author.mention)
