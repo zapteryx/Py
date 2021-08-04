ffc20c54ef813a1adcb19764dc9095cc58e0ccec
Max <max@zptx.icu>
2021-08-04 12:29:04 +0800

    implement db, QoL changes

--- main.py
+++ main.py
@@ -5,6 +5,30 @@ import os
 bot_token = os.environ.get("bot-token")
 assert bot_token != None, "Bot token not specified."
 prefix = ',' if os.environ.get("prefix") == None else os.environ.get("prefix")
+sql_info = {"host": os.environ.get("db-host"), "username": os.environ.get("db-username"), "password": os.environ.get("db-password"), "database": os.environ.get("db-database")}
+
+for key in sql_info:
+    if sql_info[key] == None:
+        sql_info = None
+        break
+
+import mysql.connector
+
+if sql_info == None:
+    db = None
+else:
+    try:
+        db = mysql.connector.connect(
+            host=sql_info["host"],
+            user=sql_info["username"],
+            password=sql_info["password"],
+            database=sql_info["database"]
+        )
+        db.autocommit = True
+        print("Database support enabled!")
+    except mysql.connector.Error as err:
+        print("Disabling database support as an error occurred during attempt to connect:\n%s" % err)
+        db = None
 
 from discord.ext.commands import Bot
 
@@ -30,7 +54,8 @@ async def on_message(message):
     # Process the bot commands after our message actions are done
     await bot.process_commands(message)
 
-bot.add_cog(Core(bot))
+bot.add_cog(Core(bot, db))
 bot.add_cog(Reaction(bot))
 
+print('Attempting connection to Discord...')
 bot.run(bot_token)
